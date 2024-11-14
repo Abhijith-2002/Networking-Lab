5bfa70b5760c23326da552b4567dbd29636a8431
abhijith <xperio135@gmail.com>
2024-11-14 10:54:34 +0530

    improved code

--- Go-Back-N/client.c
+++ Go-Back-N/client.c
@@ -5,28 +5,28 @@
 #include<netinet/in.h>
 #include<sys/time.h>
 #include<sys/socket.h>
+#include<unistd.h>
 
-#define PORT 4003
+#define PORT 4004
 #define IP "127.0.0.1"
 #define WINDOWSIZE 4
 #define BUFFERSIZE 1024
 #define TOTALFRAMES 10
 #define TIMEOUT 2
 
-typedef struct
-{
+typedef struct {
     int seqNo;
     char buffer[BUFFERSIZE];
-}Frame;
+} Frame;
 
 int main() {
     int sockfd;
     struct sockaddr_in server_addr;
     socklen_t addrlen = sizeof(struct sockaddr_in);
-    Frame dataFrame,ackFrame;
+    Frame dataFrame, ackFrame;
     int framesize = sizeof(Frame);
 
-    if((sockfd=socket(AF_INET,SOCK_DGRAM,0))==-1) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("socket");
         exit(1);
     }
@@ -38,8 +38,9 @@ int main() {
     struct timeval tv;
     tv.tv_sec = TIMEOUT;
     tv.tv_usec = 0;
-    if(setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,&tv,sizeof(tv))<0) {
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
         perror("setsockopt");
+        close(sockfd);
         exit(1);
     }
 
@@ -47,30 +48,34 @@ int main() {
     int nextSeqNo = 0;
     Frame buffer[TOTALFRAMES];
 
-    while(base<TOTALFRAMES) {
-        while(nextSeqNo<base+WINDOWSIZE && nextSeqNo<TOTALFRAMES) {
+    while (base < TOTALFRAMES) {
+        while (nextSeqNo < base + WINDOWSIZE && nextSeqNo < TOTALFRAMES) {
             dataFrame.seqNo = nextSeqNo;
+            snprintf(dataFrame.buffer, BUFFERSIZE, "Frame data for sequence number %d", dataFrame.seqNo);
             buffer[nextSeqNo] = dataFrame;
-            sendto(sockfd,&dataFrame,framesize,0,(struct sockaddr *)&server_addr,addrlen);
-            printf("Frame-%d sent\n",nextSeqNo);
+
+            sendto(sockfd, &dataFrame, framesize, 0, (struct sockaddr *)&server_addr, addrlen);
+            printf("Frame-%d sent\n", nextSeqNo);
             nextSeqNo++;
         }
 
-        int valread = recvfrom(sockfd,&ackFrame,framesize,0,(struct sockaddr *)&server_addr,&addrlen);
-        if(valread>0) {
-            if(ackFrame.seqNo==base) {
-                printf("ACK recieved for Frame-%d\n",ackFrame.seqNo);
-                base++;
-                printf("Sliding Window to Frame-%d\n",base);
+        int valread = recvfrom(sockfd, &ackFrame, framesize, 0, (struct sockaddr *)&server_addr, &addrlen);
+        if (valread > 0) {
+            if (ackFrame.seqNo >= base) {
+                printf("ACK received for Frame-%d\n", ackFrame.seqNo);
+                base = ackFrame.seqNo + 1;
+                printf("Sliding Window to Frame-%d\n", base);
             }
-        }
-        else if(valread==-1) {
-            printf("Timeout ! Resending Frames from %d to %d\n",base,nextSeqNo-1);
-            for(int i=base;i<nextSeqNo;i++) {
-                sendto(sockfd,&buffer[i],framesize,0,(struct sockaddr *)&server_addr,addrlen);
-                printf("Frame-%d resent\n",i);
+        } else if (valread == -1) {
+            printf("Timeout! Resending Frames from %d to %d\n", base, nextSeqNo - 1);
+            for (int i = base; i < nextSeqNo; i++) {
+                sendto(sockfd, &buffer[i], framesize, 0, (struct sockaddr *)&server_addr, addrlen);
+                printf("Frame-%d resent\n", i);
             }
         }
     }
-    printf("All frames sent and acknowledged !\n");
+
+    printf("All frames sent and acknowledged!\n");
+    close(sockfd);
+    return 0;
 }
--- Go-Back-N/server.c
+++ Go-Back-N/server.c
@@ -5,30 +5,26 @@
 #include<netinet/in.h>
 #include<sys/time.h>
 #include<sys/socket.h>
-#include<sys/types.h>
 #include<unistd.h>
-#include<time.h>
 
 #define BUFFERSIZE 1024
-#define PORT 4003
+#define PORT 4004
 #define TOTALFRAMES 10
 #define LOSSPROB 0.2
 
-typedef struct
-{
+typedef struct {
     int seqNo;
     char buffer[BUFFERSIZE];
-}Frame;
-
+} Frame;
 
 int main() {
     int sockfd;
-    struct sockaddr_in server_addr,client_addr;
+    struct sockaddr_in server_addr, client_addr;
     socklen_t addrlen = sizeof(struct sockaddr_in);
-    Frame dataFrame,ackFrame;
+    Frame dataFrame, ackFrame;
     int framesize = sizeof(Frame);
 
-    if((sockfd=socket(AF_INET,SOCK_DGRAM,0))==-1) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         perror("socket");
         exit(1);
     }
@@ -37,41 +33,48 @@ int main() {
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(sockfd,(struct sockaddr *)&server_addr,addrlen)==-1) {
+    if (bind(sockfd, (struct sockaddr *)&server_addr, addrlen) == -1) {
         perror("bind");
+        close(sockfd);
+        exit(1);
     }
 
     int expectedSeqNo = 0;
 
-    while(expectedSeqNo<TOTALFRAMES) {
-        int valread = recvfrom(sockfd,&dataFrame,framesize,0,(struct sockaddr *)&client_addr,&addrlen);
-        if(valread>0) {
-            if(dataFrame.seqNo==expectedSeqNo) {
-                printf("Frame-%d recieved\n",dataFrame.seqNo);
+    while (expectedSeqNo < TOTALFRAMES) {
+        int valread = recvfrom(sockfd, &dataFrame, framesize, 0, (struct sockaddr *)&client_addr, &addrlen);
+        if (valread > 0) {
+            dataFrame.buffer[BUFFERSIZE - 1] = '\0';
+
+            if (dataFrame.seqNo == expectedSeqNo) {
+                printf("Frame-%d received: %s\n", dataFrame.seqNo, dataFrame.buffer);
                 ackFrame.seqNo = expectedSeqNo;
-                sendto(sockfd,&ackFrame,framesize,0,(struct sockaddr *)&client_addr,addrlen);
-                printf("ACK sent for Frame-%d",expectedSeqNo);
+                if ((rand() % 100) / 100.0 > LOSSPROB) {
+                    sendto(sockfd, &ackFrame, framesize, 0, (struct sockaddr *)&client_addr, addrlen);
+                    printf("ACK sent for Frame-%d\n", expectedSeqNo);
+                } else {
+                    printf("Simulating ACK loss: ACK for Frame-%d not sent!\n", expectedSeqNo);
+                }
                 expectedSeqNo++;
+            } else if (dataFrame.seqNo > expectedSeqNo) {
+                printf("Out of order Frame-%d received and discarded\n", dataFrame.seqNo);
+                ackFrame.seqNo = expectedSeqNo - 1;
+                sendto(sockfd, &ackFrame, framesize, 0, (struct sockaddr *)&client_addr, addrlen);
+                printf("ACK resent for the last correctly received Frame-%d\n", ackFrame.seqNo);
+            } else {
+                printf("Duplicate Frame-%d received\n", dataFrame.seqNo);
+                ackFrame.seqNo = dataFrame.seqNo;
+                sendto(sockfd, &ackFrame, framesize, 0, (struct sockaddr *)&client_addr, addrlen);
+                printf("ACK resent for Frame-%d\n", dataFrame.seqNo);
             }
-            else if(dataFrame.seqNo>expectedSeqNo) {
-                printf("Out of order Frame-%d recieved and discarded\n",dataFrame.seqNo);
-                ackFrame.seqNo = expectedSeqNo-1;
-                sendto(sockfd,&ackFrame,framesize,0,(struct sockaddr *)&client_addr,addrlen);
-                printf("ACK resent for the last correctly recieved Frame-%d",ackFrame.seqNo);
-            }
-            else {
-                printf("Invalid Frame recieved !");
-                ackFrame.seqNo = expectedSeqNo-1;
-                sendto(sockfd,&ackFrame,framesize,0,(struct sockaddr *)&client_addr,addrlen);
-                printf("ACK resent for the last correctly recieved Frame-%d",ackFrame.seqNo);
-            }
-        }
-        else {
+        } else {
             perror("recvfrom");
+            close(sockfd);
             exit(1);
         }
     }
-    printf("All Frames recieved successfully !\n");
+
+    printf("All frames received successfully!\n");
     close(sockfd);
     return 0;
 }
